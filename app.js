156ac97d7b273e8ceedd8bd77e3769946223c84e
Mohamed Hedeya <mohamedhadia611@gmail.com>
2020-10-21 18:51:07 +0200

    final proj

---
+++ app.js
@@ -0,0 +1,35 @@
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll("nav ul li");
+
+window.addEventListener("mousemove", cursor);
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("mouseover", (e) => {
+    mouseCursor.classList.add("cursor-grow");
+    // link.classList.add("test");
+  });
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("cursor-grow");
+    // link.classList.remove("test");
+  });
+});
+
+// testing area .....................................
+// ..................................................
+// let navLinkT = document.querySelector(".test");
+
+// function setTranslate(xPos, yPos, el) {
+//   el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+// }
+
+// navLinkT.addEventListener("mousemove", () => {
+//   let random = Math.floor(Math.random() * 5);
+//   setTranslate(random, random, navLinkT);
+// });
+// ..................................................
+// testing area .....................................
